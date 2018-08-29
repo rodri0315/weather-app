3e768ce24ff8df29a506bcb17bfc036a69d4d208
Jorge Rodriguez <rodri0315@gmail.com>
2018-08-28 21:13:08 -0400

    First commit, setup file tree, install OnsenUI.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+import ons from 'onsenui';
+import {Page} from 'react-onsenui';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Page>
+          <h3>Weather App!</h3>
+        </Page>
+      </div>
+    );
+  }
+}
