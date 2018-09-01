7d3292da16518035030f5fc56ad444c23bde188c
Jorge Rodriguez <rodri0315@gmail.com>
2018-09-01 15:37:17 -0400

    Add redux and tools, axios, initial setup.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'onsenui/css/onsenui.css';
 import 'onsenui/css/onsen-css-components.css';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+import rootReducer from "./reducers/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(rootReducer)}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
 
